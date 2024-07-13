7586bde7157a2e97201e1d072db41abeee53c57d
MembaMcetywa <iamemba10@gmail.com>
2024-07-13 12:30:46 +0200

    added base game component to act as container for the different components

---
+++ src/components/GameComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Game Screen</h2>
+    <div v-if="questions.length > 0">
+      <div v-for="(question, index) in questions" :key="index">
+        <p>{{ question }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useGameDetailsStore } from '../stores/gamestore'
+
+const { questions } = useGameDetailsStore()
+
+console.log('Questions in GameComponent:', questions)
+</script>
+
+<style scoped></style>
