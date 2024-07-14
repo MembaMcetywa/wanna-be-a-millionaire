c35e3e7503bddc1d04cf91ee7a14b3f7b0892375
MembaMcetywa <iamemba10@gmail.com>
2024-07-14 10:15:51 +0200

    added base functionality to handle user choosing correct answer

--- src/components/GameComponent.vue
+++ src/components/GameComponent.vue
@@ -1,20 +1,53 @@
 <template>
   <div>
-    <h2>Game Screen</h2>
-    <div v-if="questions.length > 0">
-      <div v-for="(question, index) in questions" :key="index">
-        <p>{{ question }}</p>
-      </div>
+    <div v-if="currentQuestion">
+      <QuestionComponent :question="currentQuestion" @answerSelected="handleAnswer" />
+    </div>
+    <div v-else>
+      <h2>Game over! Your score: {{ correctAnswers }}</h2>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, computed, watchEffect } from 'vue'
 import { useGameDetailsStore } from '../stores/gameDetailsStore'
+import QuestionComponent from './QuestionComponent.vue'
+import type { Question } from '../utils/types'
 
-const { questions } = useGameDetailsStore()
+const { totalScore, questions, score, increaseCorrectAnswers, increaseWinnings } =
+  useGameDetailsStore()
 
-console.log('Questions in GameComponent:', questions)
+const currentIndex = ref(0)
+const correctAnswers = ref(0)
+
+const currentQuestion = computed((): Question | null => {
+  return questions[currentIndex.value] || null
+})
+
+function handleAnswer(selectedOption: string) {
+  if (!currentQuestion.value) return
+
+  if (selectedOption === currentQuestion.value.correctAnswer) {
+    correctAnswers.value++
+    increaseCorrectAnswers()
+
+    if (currentIndex.value < questions.length - 1) {
+      currentIndex.value++
+    } else {
+      alert('quiz ended')
+    }
+  }
+}
+
+watchEffect(() => {
+  console.log('Current state:', {
+    questions: questions,
+    score: score,
+    correctAnswers: correctAnswers.value,
+    totalScore: totalScore
+  })
+})
 </script>
 
 <style scoped></style>
