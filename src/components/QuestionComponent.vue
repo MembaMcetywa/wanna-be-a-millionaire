d27baff5089618b973a2734dd2fab1cc949130d6
MembaMcetywa <iamemba10@gmail.com>
2024-07-14 10:10:12 +0200

    added base question component

---
+++ src/components/QuestionComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="question">
+    <h2>{{ question.question }}</h2>
+    <ul>
+      <li v-for="option in question.options" :key="option" @click="selectOption(option)">
+        {{ option }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+import type { Question } from '../utils/types'
+
+const props = defineProps<{
+  question: Question
+}>()
+
+const emits = defineEmits<{
+  (event: 'answerSelected', answer: string): void
+}>()
+
+function selectOption(option: string) {
+  emits('answerSelected', option)
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+li {
+  cursor: pointer;
+  margin: 5px 0;
+  background-color: #f0f0f0;
+  padding: 10px;
+  border-radius: 5px;
+  color: black;
+}
+
+li:hover {
+  background-color: #e0e0e0;
+}
+</style>
