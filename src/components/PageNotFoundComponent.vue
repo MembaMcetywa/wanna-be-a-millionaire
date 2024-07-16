421083d880e56243a350f0bf66594d437f7861b3
MembaMcetywa <iamemba10@gmail.com>
2024-07-16 18:46:10 +0200

    added page not found component to be used as a catch for incorrect routes

---
+++ src/components/PageNotFoundComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="game-over-container">
+    <h2>Oops! Wrong Page</h2>
+    <p>This page doesn't exist. Return home to play a game.</p>
+    <div class="game-over-container-actions">
+      <CustomButton @click="navigateHome">Go Home</CustomButton>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { useGameDetailsStore } from '../stores/gameDetailsStore'
+import CustomButton from '../components/CustomButton.vue'
+
+const store = useGameDetailsStore()
+const router = useRouter()
+
+function navigateHome() {
+  store.endGame()
+  router.push({ name: 'home' })
+}
+</script>
+<style>
+.game-over-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+.game-over-container h2 {
+  font-weight: 600;
+  transition: color 0.3s ease;
+}
+
+.game-over-container h2:hover {
+  text-decoration: line-through;
+  cursor: pointer;
+  color: red;
+}
+</style>
